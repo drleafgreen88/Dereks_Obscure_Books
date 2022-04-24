a73fe11edbde386ace85a04ba13f5085d82bf9ab
Derek Leafgreen <drl21788@gmail.com>
2022-04-24 10:44:29 +0100

    work continuing

--- db/my_book_store.sql
+++ db/my_book_store.sql
@@ -1,14 +1,24 @@
--- DROP TABLE IF EXISTS books;
--- DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS publishers;
+DROP TABLE IF EXISTS books;
 
--- CREATE TABLE authors (
---   id SERIAL PRIMARY KEY,
---   first_name VARCHAR(255),
---   last_name VARCHAR(255)
--- );
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
 
--- CREATE TABLE books (
---   id SERIAL PRIMARY KEY,
---   title VARCHAR(255),
---   author_id INT NOT NULL REFERENCES authors(id)
--- );
+CREATE TABLE publishers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+)
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    publisher_id INT NOT NULL REFERENCES publishers(id),
+    author_id INT NOT NULL REFERENCES authors(id),
+    genre VARCHAR(255),
+    buying_price INT NOT NULL,
+    selling_price INT NOT NULL
+);
--- models/book.py
+++ models/book.py
@@ -1,8 +1,11 @@
 class Book:
-    def __init__(self, title, author, genre, id = None):
-        self.title=title
-        self.author=author
+    def __init__(self, title, publisher, author, genre, buying_price, selling_price, id = None):
+        self.title = title
+        self.publisher = publisher
+        self.author = author
         self.genre = genre
-        self.id=id
+        self.buying_price = buying_price
+        self.selling_price = selling_price
+        self.id = id
 
 #Do we need genre if all the books are Naturalist fiction?
