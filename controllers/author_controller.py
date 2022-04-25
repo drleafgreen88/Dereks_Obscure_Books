f09ac51bd6233ddf584700f5f251688d07ca1f0e
Derek Leafgreen <drl21788@gmail.com>
2022-04-25 17:15:12 +0100

    can add new authors

--- controllers/author_controller.py
+++ controllers/author_controller.py
@@ -24,3 +24,17 @@ def update_author(id):
 def edit_author(id):
     author = author_repository.select(id)
     return render_template('authors/edit.html', author=author)
+
+# NEW
+@author_bp.route("/authors/new")
+def new_author():
+    return render_template("authors/new.html")
+
+# CREATE
+@author_bp.route("/authors", methods=["POST"])
+def create_author():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    new_author = Author(first_name, last_name)
+    author_repository.save(new_author)
+    return redirect("/authors")
