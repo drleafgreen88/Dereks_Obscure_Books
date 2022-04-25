2de212caa48784fc1c19926c145bd5dd638a7c53
Derek Leafgreen <drl21788@gmail.com>
2022-04-25 15:47:29 +0100

    edit working on publishers

--- controllers/publisher_controller.py
+++ controllers/publisher_controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, Blueprint, request
+from models.publisher import Publisher
 
 import repositories.author_repository as author_repository
 import repositories.book_repository as book_repository
@@ -11,3 +12,17 @@ def publishers():
     publishers = publisher_repository.select_all()
     print (publishers)
     return render_template("publishers/index.html", publishers = publishers)
+
+# UPDATE
+@publisher_bp.route("/publishers/<id>", methods=["POST"])
+def update_publisher(id):
+    name = request.form["name"]
+    publisher = Publisher(name, id)
+    publisher_repository.update(publisher)
+    return redirect("/publishers")
+
+# EDIT
+@publisher_bp.route("/publishers/<id>/edit")
+def edit_publisher(id):
+    publisher = publisher_repository.select(id)
+    return render_template('publishers/edit.html', publisher=publisher)
