2b92b262dffee6dcb74a44916090efd05491c737
Derek Leafgreen <drl21788@gmail.com>
2022-04-26 21:34:37 +0100

    MVP reached and presentation tidied

--- models/book.py
+++ models/book.py
@@ -7,7 +7,17 @@ class Book:
         self.buying_price = buying_price
         self.selling_price = selling_price
         self.stock_quantity = stock_quantity
+        self.stock_level = get_stock_level(int(stock_quantity))
         self.id = id
+
+def get_stock_level(stock_quantity):
+    stock_level = ""
+    if stock_quantity == 0:
+        stock_level =  "OUT OF STOCK"
+    elif stock_quantity <= 5:
+        stock_level = "LOW STOCK" 
+
+    return stock_level
 
 # Could create a new function to set the stock level and call it as part of creating your object instance in the constructor (init) function above
 #Do we need genre if all the books are Naturalist fiction?
